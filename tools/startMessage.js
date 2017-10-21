d6b53cc4203b6ca33bd1f420ffd051712594d691
Rushil Basappa <rushil.oucse37@gmail.com>
2017-10-21 20:55:04 +0530

    Environment setup
    create package.json
    create src directory
    set up webpack
    create editorconfig
    set up babel
    set up express web server
    set up eslint
    add npm scripts and startMessage
    set up testing - mocha
    set up travis ci

---
+++ tools/startMessage.js
@@ -0,0 +1,5 @@
+import colors from 'colors';
+
+console.log('====================================');
+console.log('Starting in dev mode...'.green);
+console.log('====================================');
